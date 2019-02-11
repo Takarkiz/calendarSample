bd533a38545c4a6345bc7bf2ab59e11a7c464bba
Takarkiz <ponpontanuki16@gmail.com>
2019-02-11 10:26:02 +0900

    first commit

---
+++ app/src/main/java/com/takhaki/calendars/MainActivity.java
@@ -0,0 +1,57 @@
+package com.takhaki.calendars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView titleText;
+    private Button prevButton, nextButton;
+    private CalendarAdapter mCalendarAdapter;
+    private GridView calendarGridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        titleText = findViewById(R.id.titleText);
+        prevButton = findViewById(R.id.prevButton);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarAdapter.prevMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
+            }
+        });
+        nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarAdapter.nextMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
+            }
+        });
+        calendarGridView = findViewById(R.id.calendarGridView);
+        mCalendarAdapter = new CalendarAdapter(this);
+        calendarGridView.setAdapter(mCalendarAdapter);
+
+        calendarGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Test", "Position: "+String.valueOf(position)+", id: "+String.valueOf(id));
+                Toast.makeText(MainActivity.this, "Clicked day is " + mCalendarAdapter.getDateItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        titleText.setText(mCalendarAdapter.getTitle());
+    }
+
+}
